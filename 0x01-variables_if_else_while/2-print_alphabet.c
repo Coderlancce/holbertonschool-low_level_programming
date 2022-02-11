9920ec4da8eb64b48a3dccd60bbe6c0c7f7640cf
Coderlancce <coderlancce@gmail.com>
2022-02-11 10:09:10 -0500

    2 task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - the name of the function
+ * while: conditional iteration
+ * Return: The return its 0 
+ */
+
+int main(void)
+{
+	int lowercase = 97;
+
+	while (lowercase < 123)
+	{
+		putchar(lowercase);
+		lowercase++;
+	}
+	putchar('\n');
+	return (0);
+}
