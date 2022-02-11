b35dc71600d7105661cb51dc0e5a711c5a39c483
Coderlancce <coderlancce@gmail.com>
2022-02-11 15:23:42 -0500

    6 task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - name of the function
+ * for: cicle for 0 - 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 48; numbers < 58; numbers++)
+		putchar (numbers);
+
+	putchar ('\n');
+	return (0);
+}
