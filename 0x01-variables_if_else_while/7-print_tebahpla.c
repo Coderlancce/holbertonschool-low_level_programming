7e9568756030badd8d20392eb5b3adaa3f046124
Coderlancce <coderlancce@gmail.com>
2022-02-11 15:37:04 -0500

    7 task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - name of the function
+ * for: cicle reverse lowercase
+ * Return: 0
+ */
+
+int main(void)
+{
+	int lowercase;
+
+	for (lowercase = 122; lowercase > 96; lowercase--)
+		putchar(lowercase);
+
+	return (0);
+}
