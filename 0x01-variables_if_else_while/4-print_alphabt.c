b3f21eda915acdd969ba7c82971a4022f333aec1
Coderlancce <coderlancce@gmail.com>
2022-02-11 12:36:13 -0500

    4 task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * for: cicle of lowercase
+ * Return: 0
+ */
+
+int main(void)
+{
+	for (lowercase = 97; lowercase < 123 ; lowercase++)
+	{
+		if (lowercase != 113 && lowercase != 101)
+		{
+			putchar(lowercase);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
