41c83bbd956694f3aced4b6c4999760732c75e12
Coderlancce <coderlancce@gmail.com>
2022-02-15 14:00:51 -0500

    13 task

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ */
+
+int main(void)
+{
+	int num, sum;
+
+	sum = 0;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if (num % 3 == 0 || num % 5 == 0)
+		sum += num;
+	}
+	_putchar((sum / 100000) + '0');
+	_putchar(((sum / 10000) % 10) + '0');
+	_putchar(((sum / 1000) % 10) + '0');
+	_putchar(((sum / 100) % 10) + '0');
+	_putchar(((sum / 10) % 10) + '0');
+	_putchar((sum % 10) + '0');
+}
