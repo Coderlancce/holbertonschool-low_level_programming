9568a8c0a5ab7cdef38ce8c4b163f77a1d40f843
Coderlancce <coderlancce@gmail.com>
2022-02-14 20:19:58 -0500

    7 task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - name of the function
+ * Return: last digit
+ */
+
+int print_last_digit(int r)
+{
+  return(_abs(r % 10));
+}
+
+/**
+ * _abs - name of the function
+ * Return: positive number
+ */
+
+int _abs(int n)
+{
+  return ((n < 0) ? -n : n)
+}
