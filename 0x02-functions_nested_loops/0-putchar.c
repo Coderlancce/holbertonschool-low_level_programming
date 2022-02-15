28f6174ca3c423f4132dc4363c751a2188253b13
Coderlancce <coderlancce@gmail.com>
2022-02-15 09:04:03 -0500

    0 task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,13 +5,13 @@
  * Return: 0
  */
 
-int main(void)
+int _putchar(char c)
 {
 	char c[] = "_putchar";
 	int i = 0;
 
 	while (c[i] != '\0')
-		_putchar(c[i++];
+		_putchar(c[i++]);
 
 	_putchar('\n');
 	return (0);
