7a271d937eaec577232e022c950aa0320387447b
Coderlancce <coderlancce@gmail.com>
2022-02-15 10:58:22 -0500

    10 task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add -  name of the function
+ * @a: value of ingrese
+ * @b: value of ingrese
+ * Return: a add b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
