2a2500f7ee472e5ad16337c20c8e1862bab7501e
Coderlancce <coderlancce@gmail.com>
2022-02-15 16:16:05 -0500

    14 task

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - name of the function
+ * Return: always 0
+ */
+
+int main(void)
+{
+	long int sum;
+	long int one, two;
+	int count;
+
+	sum = 0;
+	one = 0;
+	two = 1;
+	count = 0;
+
+	while (count < 49)
+	{
+		sum = one + two;
+		printf("%li, ", sum);
+		one = two;
+		two = sum;
+		count++;
+	}
+	sum = one + two;
+	printf ("li\n", sum;
+
+	return (0);
+}
