b17d1c1aaa1a3dc5c5b31524bc05686a9a561bb7
Coderlancce <coderlancce@gmail.com>
2022-02-15 10:00:35 -0500

    7 task

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,17 +3,19 @@
 
 /**
  * print_last_digit - name of the function
+ * @r: value of ingrese
  * Return: last digit
  */
 
 int print_last_digit(int r)
 {
 	_putchar(_abs(r % 10) + '0');
-	return(_abs(r % 10));
+	return (_abs(r % 10));
 }
 
 /**
  * _abs - name of the function
+ * @n: value of ingrese
  * Return: positive number
  */
 
