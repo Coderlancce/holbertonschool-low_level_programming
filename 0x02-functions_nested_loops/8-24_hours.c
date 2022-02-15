827d1766448d8f00eb306f1057712e741a5ae881
Coderlancce <coderlancce@gmail.com>
2022-02-14 20:29:09 -0500

    8 task

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *
+ *
+ */
+
+void jack_bauer(void)
+{
+	int h = 0, m;
+
+	while(h < 24)
+	{
+		m = 0;
+		while (m < 60)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+			m++;
+		}
+		h++;
+	}
+}
