951c40523067d0cdf99f695b8cc49b1314f46445
Coderlancce <coderlancce@gmail.com>
2022-02-15 09:33:11 -0500

    6 task

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,10 +3,11 @@
 
 /**
  * _abs - name of the function
+ * @r: value of ingrese
  * Return: short if else
  */
 
-int _abs(int);
+int _abs(int r);
 {
-  return((r < 0) ? -r : r);
+	return ((r < 0) ? -r : r);
 }
