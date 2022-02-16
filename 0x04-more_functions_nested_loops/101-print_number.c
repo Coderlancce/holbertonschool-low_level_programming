98886920f99dd219a66e2cfca1ad45376ed6bc87
Coderlancce <coderlancce@gmail.com>
2022-02-16 16:15:29 -0500

    12 task

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - name of the function
+ * if: conditional print
+ * Return: none
+ */
+
+void print_number(int n)
+{
+	if (n / 1000 != 0)
+	{
+		_putchar((n / 1000) + '0');
+		_putchar(((n / 100) % 10) + '0');
+		_putchar(((n / 10) % 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if(n < 0)
+	{
+		_putchar('-');
+		_putchar(((-n / 10) % 10) + '0');
+		_putchar((-n % 10) + '0');
+	}
+	else if (n / 100 != 0)
+	{
+		_putchar(((n / 100) % 10) + '0');
+		_putchar(((n / 10) % 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if (n / 10 != 0)
+	{
+		_putchar(((n / 10) % 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else
+		_putchar((n % 10) + '0');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -15,3 +15,5 @@ void print_line(int n);
 void print_diagonal(int n);
 
 void print_triangle(int size);
+
+void print_number(int n);
