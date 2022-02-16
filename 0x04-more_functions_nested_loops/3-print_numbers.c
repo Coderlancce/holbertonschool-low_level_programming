077110e0596a61f0a88d2ea7ab6bcbac988565db
Coderlancce <coderlancce@gmail.com>
2022-02-16 10:33:30 -0500

    3 task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - name of the function
+ * for: cicle of print
+ * Return: none
+ */
+
+void print_numbers(void)
+{
+	int cont;
+
+	for (cont = 0; cont < 10; cont++)
+		_putchar (cont + '0');
+
+	_putchar ('\n');
+}
