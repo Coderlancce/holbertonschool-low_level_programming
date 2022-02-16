d3fa5a755d1434a78b94a4439a203dd79c9140c8
Coderlancce <coderlancce@gmail.com>
2022-02-16 11:23:10 -0500

    6 task

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - name of the function
+ * @n: value of ingrese
+ * Return: none
+ */
+
+void print_line(int n)
+{
+	int c = 0;
+
+	if(n > 0)
+	{
+		while (c <= n)
+		{
+			_putchar ('_');
+			c++;
+		}
+		_putchar ('\n');
+		}
+		else
+			_putchar('\n');
+}
