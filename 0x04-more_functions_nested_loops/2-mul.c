0d459433e84e623294d9a509bb50b02a4152127c
Coderlancce <coderlancce@gmail.com>
2022-02-16 10:21:12 -0500

    2 task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * mul - name of the function
+ * @a: value of ingrese
+ * @b: value of ingrese
+ * Return: mul
+ */
+
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return(c);
+}
