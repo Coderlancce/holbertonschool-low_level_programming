52ea6f81d3d79a26321b006d6880df2e82c2e053
Coderlancce <coderlancce@gmail.com>
2022-02-16 15:15:52 -0500

    10 task

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - name of the function
+ * @size: value of ingrese
+ * Return: none
+ */
+
+void print_triangle(int size)
+{
+	int x, y = 0;
+	int space;
+
+	if (size > 0)
+	{
+		while (y < size)
+		{
+			space = size - y - 1;
+			while (x < size)
+			{
+				if (space > x)
+				{
+					putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+				x++;
+			}
+			y++;
+			x = 0;
+			_putchar ('\n');
+		}
+	}
+	else
+	_putchar('\n');
+}
