ae23d56727fe5cb4c8ba19a2dcbad1a705a340d5
Coderlancce <coderlancce@gmail.com>
2022-02-16 12:02:19 -0500

    8 task

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_aquare - name of the function
+ * @size: value of ingrese
+ * Return: none
+ */
+
+void print_square(int size)
+{
+	int a = 0;
+	int b = 0;
+
+	if (size > 0)
+	{
+		while (a < size)
+		{
+			while (b < size)
+			{
+				_putchar('#');
+				b++;
+			}
+			a++;
+			b = 0;
+			_putchar('\n');
+		}
+	}
+	else
+	_putchar('\n');
+}
