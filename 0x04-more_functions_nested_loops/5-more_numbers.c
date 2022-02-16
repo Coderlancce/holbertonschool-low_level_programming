d51334ffc1ee3260b938fff075e8aede9fa837a3
Coderlancce <coderlancce@gmail.com>
2022-02-16 11:06:34 -0500

    5 task

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - name of the function
+ * Return: none
+ */
+
+void more_numbers(void)
+{
+	int cicle;
+	int cont;
+
+	for (cont = 0; cont < 10; cont++)
+	{
+		for (cicle = 0; cicle < 15; cicle++)
+		{
+			if (cicle >= 10)
+			{
+				_putchar ((cicle / 10) + '0');
+			}
+			_putchar ((cicle % 10) + '0');
+		}
+		_putchar ('\n');
+	}
+}
