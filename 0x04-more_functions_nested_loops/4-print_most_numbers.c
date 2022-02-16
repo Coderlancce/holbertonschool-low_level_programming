1a422db2b38eb7a89a6c0afc880598f30552188e
Coderlancce <coderlancce@gmail.com>
2022-02-16 10:49:49 -0500

    4 task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - name of the function
+ * Return: none
+ */
+
+void print_most_numbers(void)
+{
+	int cicle;
+
+	for (cicle = 0; cicle < 10; cicle++)
+	{
+		if (cicle != 4 && cicle != 2)
+		_putchar (cicle + '0');
+	}
+	putchar ('\n');
+}
