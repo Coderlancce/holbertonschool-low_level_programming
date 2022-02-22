3c24c37ff86981c2533423b4f19c7037ee9b5494
Coderlancce <coderlancce@gmail.com>
2022-02-22 15:39:09 -0500

    1 task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int - name of the function
+ * @a: value of ingrese
+ * @b: value of ingrese
+ * Return: none
+ */
+
+void swap_int(int *a, int *b)
+{
+	c = *a;
+	*a = *b;
+	*b = c;
+}
