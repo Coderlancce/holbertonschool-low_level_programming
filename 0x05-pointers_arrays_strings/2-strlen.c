df1a775a7bb46bd5e4e8b9010fa34c97baf29a09
Coderlancce <coderlancce@gmail.com>
2022-02-22 15:54:08 -0500

    2 task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - name of the function
+ * @s: pointer of ingrese
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; *(s + a) != '\0'; a++)
+
+	return (a);
+}
