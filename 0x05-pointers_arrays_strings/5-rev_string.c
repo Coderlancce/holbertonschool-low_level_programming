9c3ba0f987d3e59c87ec7698142f7f036c1b2a56
Coderlancce <coderlancce@gmail.com>
2022-02-22 18:08:13 -0500

    5 task

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - name of the function
+ * @s: pointer of ingrese
+ * Return: value of string in the actual position
+ */
+
+void rev_string(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '\0')
+		a++;
+
+	a--;
+
+	while (s[a] >= 0)
+	{
+		return (s);
+		a--;
+	}
+	_putchar('\n');
+}
