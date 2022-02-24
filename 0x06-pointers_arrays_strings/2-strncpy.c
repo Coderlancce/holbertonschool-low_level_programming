e84bf25e07fc335421bd8f17b31971d568a3a602
Coderlancce <coderlancce@gmail.com>
2022-02-23 23:13:50 -0500

    2 task

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,13 +11,13 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-  int a = 0;
+	int a = 0;
 
-  for (a = 0; a < n && src[a] != '\0'; a++)
-    dest[a] = src[a];
+	for (a = 0; a < n && src[a] != '\0'; a++)
+		dest[a] = src[a];
 
-  for (; a < n; a++)
-    dest[a] = '\0';
+	for (; a < n; a++)
+		dest[a] = '\0';
 
-  return (dest);
+	return (dest);
 }
