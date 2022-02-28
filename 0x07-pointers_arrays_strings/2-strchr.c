dc3af3ceda2b35bb17461dbdd5b30f864e214dad
Coderlancce <coderlancce@gmail.com>
2022-02-28 12:44:34 -0500

    2 task

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,12 +9,15 @@
 
 char *_strchr(char *s, char c)
 {
-  int a;
-  
-  for (a = 0; s[a] != '\0'; a++)
-    {
-    if (s[a] == c)
-      return (s[a]);
-    }
-  return (s[a] == c ? s[a] : '\0');
+	unsigned int a = 0;
+
+	for (; *(s + a) != '\0'; a++)
+	{
+		if (*(s + a) == c)
+			return (s + a);
+	}
+	if (*(s + a) == c)
+		return (s + a);
+
+	return ('\0');
 }
