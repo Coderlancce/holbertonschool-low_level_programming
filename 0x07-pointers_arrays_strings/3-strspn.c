c53b3f06929711637cd96cd1b6824be9dc8c3706
Coderlancce <coderlancce@gmail.com>
2022-02-28 13:32:40 -0500

    3 task

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,17 +9,17 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-  unsigned int a, b;
+	unsigned int a, b;
 
-  for (a = 0; s[a]; a++)
-    {
-      for (b = 0; accept[b]; b++)
+	for (a = 0; s[a]; a++)
 	{
-	  if (s[a] == accept[b])
-	    break;
+		for (b = 0; accept[b]; b++)
+		{
+			if (s[a] == accept[b])
+				break;
+		}
+		if (!(accept[b]))
+			break;
 	}
-      if (!(accept[b]))
-	break;
-    }
-  return (a);
+	return (a);
 }
