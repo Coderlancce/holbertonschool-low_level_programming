e36f7d31ef48ab5d60eac5d739cdb020f4bf52d4
Coderlancce <coderlancce@gmail.com>
2022-03-02 12:26:30 -0500

    1 task

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - name of the function
+ * @s: pointer to array
+ * Return: reverse array of pointer s
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (!s || !*s)
+		return;
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
