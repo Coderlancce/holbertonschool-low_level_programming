8e4c51d0afdb44291e988174a90aefc7f072ad42
Coderlancce <coderlancce@gmail.com>
2022-03-02 15:56:08 -0500

    6 task

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int pow_recursion(int a, int n);
+int is_prime_number(int n);
+int cal_prim(int a, int n);
 
 #endif /*MAIN_H*/
