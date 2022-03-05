fdc024cbc1024726a1b405cc982182e1f3d81b15
Coderlancce <coderlancce@gmail.com>
2022-03-05 13:11:04 -0500

    3 task

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,10 +9,10 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc < 3 || argc > 3)
-	{
-		printf("%s\n", "Error");
-		return (0);
-	}
-	return (printf("%d\n", atoi(argv[1]) * atoi(argv[2])));
+	if (argc == 3)
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	else
+		printf("Error\n");
+
+	return (0);
 }
