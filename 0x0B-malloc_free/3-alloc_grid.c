25c6a7c66d87ffe8420f99d6503d320ec4f30265
Coderlancce <coderlancce@gmail.com>
2022-03-08 17:59:35 -0500

    3 task

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,15 +24,17 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		a[i] = malloc(sizeof(int) * width);
-		if (a == NULL)
+		array[i] = (int *)malloc(sizeof(int) * width);
+
+		if (array[i] == NULL)
 		{
-			free(a);
+			for (j = 0; j < i; j++)
+				free(array[j]);
+			free(array);
 			return (NULL);
 		}
-
 		for (j = 0; j < width; j++)
-			a[i][j] = 0;
+		array[i][j] = 0;
 	}
 
 	return (a);
