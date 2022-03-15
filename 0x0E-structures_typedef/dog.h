d212b33e58c9f99da00148bd55cb11865754705c
Coderlancce <coderlancce@gmail.com>
2022-03-15 08:07:01 -0500

    0 task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,12 +10,11 @@
  * @owner: pointer to owner of dog
  */
 
-struct dog {
-
-  char *name;
-  float age;
-  char *owner;
-
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
