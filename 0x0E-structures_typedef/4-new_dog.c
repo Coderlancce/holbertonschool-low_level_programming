287a862ec6b331f7acfecec6e5faf94a8d1cf4fe
Coderlancce <coderlancce@gmail.com>
2022-03-15 12:07:21 -0500

    4 task

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,42 @@
 #include "dog.h"
 
 /**
+ * strcpy - name of the function
+ * @s1: pointer to clear table
+ * @s2: pointer to white table
+ * Return: s1 with information s2
+ */
+
+char strcpy(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s2[i]; i++)
+		s1[i] = s2[i];
+
+	s1[i] = '\0';
+	return (s1);
+}
+
+
+/**
+ * count - name of the function
+ * @s: pointer to information
+ * Return: number of strings
+ */
+
+int count(char *s)
+{
+	int a;
+
+	for (a = 0; s[a]; a++)
+		;
+
+	return (a);
+}
+
+
+/**
  * new_dog - name of the function
  * @name: pointer with name
  * @age: age of the dog
@@ -12,13 +48,29 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog2;
 
+	if (!name || !owner || age < 0)
+		return (NULL);
 	dog2 = malloc(sizeof(dog_t));
 	if (!dog2)
 		return (NULL);
 
-	dog2->name = name;
+	dog2->name = malloc(count(name) + 1);
+	if (!dog2->name)
+	{
+		free(dog2);
+		return (NULL);
+	}
+
+	dog2->owner = malloc(count(owner) + 1);
+	if (!dog2->owner)
+	{
+		free(dog2->name);
+		free(dog2);
+		return (NULL);
+	}
+	dog2->name = strcpy(dog->name, name);
 	dog2->age = age;
-	dog2->owner = owner;
+	dog2->owner = strcpy(dog2->owner, owner);
 
 	return (dog2);
 }
