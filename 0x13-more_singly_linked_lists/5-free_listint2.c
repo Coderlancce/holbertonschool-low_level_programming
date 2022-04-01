991c64ffe08690789cfdffcd79be6e44e7c23380
Coderlancce <coderlancce@gmail.com>
2022-04-01 10:14:29 -0500

    5 task

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,10 +9,10 @@
 
 void free_listint2(listint_t **head)
 {
-	if (!*head || !head)
+	if (*head == NULL || head == NULL)
 		return;
 
-	free_listint2((*head)->next);
+	free_listint2(&(*head)->next);
 	free(*head);
 	*head = NULL;
 }
