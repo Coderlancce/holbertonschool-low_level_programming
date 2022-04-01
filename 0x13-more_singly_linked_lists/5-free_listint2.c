f16047162a764126e1796db251d84db612ef8022
Coderlancce <coderlancce@gmail.com>
2022-04-01 09:54:09 -0500

    5 task

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - funtion free start in doble pointer head
+ * @head: doble pointer to pointer head
+ *
+ * Return: none
+ */
+
+void free_listint2(listint_t **head)
+{
+	if (!*head || !head)
+		return;
+
+	free_listint2(&(*head)->next);
+	free(*head);
+}
