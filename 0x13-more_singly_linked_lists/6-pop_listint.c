f6edd478e2fdf6bd7ae6817be3f9d709cbda9500
Coderlancce <coderlancce@gmail.com>
2022-04-01 19:38:44 -0500

    6 task

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Removing the first item of a list
+ * @head: doble pointer to pointer head
+ *
+ * Return: Value of a node delete
+ */
+
+int pop_listint(listint_t **head)
+{
+	int value = 0;
+	listint_t *next_node = NULL;
+
+	if (*head == NULL)
+	{
+		return (0);
+	}
+
+	next_node = (*head)->next;
+	value = (*head)->n;
+	free(*head);
+	*head = next_node;
+
+  return (value);
+}
