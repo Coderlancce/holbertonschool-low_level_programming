bca253ca49d08adab211c491aa2da07d2194891e
Coderlancce <coderlancce@gmail.com>
2022-04-01 20:46:54 -0500

    9 task

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,44 +1,67 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - sum the values of the list.
- * @head: A pointer to the first node of the list
- * @idx: index of the list.
- * @n: value of number.
+ *insert_nodeint_at_index -  inserts a new node at a given position
+ *@head: double pointer to head node
+ *@idx: index of the list where new node should be added
+ *@n: value of the first member of new node
  *
- * Return: Value of a node index.
+ *Return: the address of the new node, or NULL if it failed
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 1;
-	listint_t *index_node = *head;
-	listint_t *insert;
-	listint_t *h = *head;
+	unsigned int i;
+	listint_t *tmp_node;
+	listint_t *new_node;
 
 	if (!head)
 		return (NULL);
-	while (i < idx)
-	{
-		index_node = (*head)->next;
-		*head = index_node;
-		++i;
-	}
-	insert = malloc(sizeof(listint_t));
 
-	if (!insert)
-		return (NULL);
-	insert->n = n;
 	if (idx == 0)
 	{
-		insert->next = *head;
-		*head = insert;
+		tmp_node = add_nodeint(head, n);
+		return (tmp_node);
 	}
-	else
+
+	tmp_node = *head;
+	i = 1;
+	while (i < idx)  /* travers till idx and get the node to insert */
 	{
-		insert->next = (*head)->next;
-		(*head)->next = insert;
-		*head = h;
+		tmp_node = tmp_node->next;
+		if (!tmp_node)
+			return (NULL);
+			i++;
 	}
-	return (insert);
+
+	new_node = malloc(sizeof(listint_t));  /* allocate new node */
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;                      /* set value in new node */
+	new_node->next = tmp_node->next; /* link on the right */
+	tmp_node->next = new_node;   /* link on the left side */
+	return (new_node);
+}
+
+
+/**
+ *add_nodeint - adds a new node at the beginning of a listint_t list.
+ *@head : pointer to head node
+ *@n: value of the first member of the structure
+ *
+ *Return: the address of the new element, or NULL if it failed
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 }
