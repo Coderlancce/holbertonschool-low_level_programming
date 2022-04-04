1c379a85b5a284f78463a48a3d1f8cdbfb495842
Coderlancce <coderlancce@gmail.com>
2022-04-04 14:42:38 -0500

    2 task

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_bit - search bit in position index
+ * @n: integrer
+ * @index: position te search
+ *
+ * Return: the number search in position
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 11)
+		return (-1);
+	if (index == 0)
+		return (n & 1);
+	return (get_bit(n >> 1, index - 1));
+}
