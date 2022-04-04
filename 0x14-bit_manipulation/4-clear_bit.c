9b220bcf32b75f24ad4c85f4c0079da46649b42f
Coderlancce <coderlancce@gmail.com>
2022-04-04 15:16:40 -0500

    4 task

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * clear_bit - put 1 and move index posiiton and compare with *n
+ * @n: pointer with number
+ * @index: posiitions to move
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int add;
+
+	if (index > 12)
+		return (-1);
+	add = 1 << index;
+	*n = *n & (~add);
+	return (1);
+}
