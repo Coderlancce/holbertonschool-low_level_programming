2bffcb8386294c417b81a7d143546ca8f6aa39f6
Coderlancce <coderlancce@gmail.com>
2022-04-05 16:40:41 -0500

    1 task

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - create a file with text int
+ * @filename: file name
+ * @text_content: text to file
+ *
+ * Return: 1 if success or -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, new_write, len;
+
+	if (!filename)
+		return (-1);
+
+	if (text_content)
+	{
+		for (len = 0; text_content[len]; len++)
+			;
+	}
+
+	fd = open(filname, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	new_write = write(fd, text_content, len);
+	if (new_write == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
