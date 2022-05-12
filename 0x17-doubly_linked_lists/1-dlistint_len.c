010843150a9ce3f65e12dc40714e9588ecbfe589
Coderlancce <coderlancce@gmail.com>
2022-05-12 09:02:38 -0500

    1 task

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -11,9 +11,9 @@ size_t dlistint_len(const dlistint_t *h)
 {
 	size_t i;
 
-	if(!h)
-		return(0);
-	for(i = 0; h != NULL; i++)
+	if (!h)
+		return (0);
+	for (i = 0; h != NULL; i++)
 		h = h->next;
-	return(i);
+	return (i);
 }
