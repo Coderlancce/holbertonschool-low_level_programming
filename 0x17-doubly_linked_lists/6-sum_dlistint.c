eefb26f3933e7513a0b393557505e91b4450af04
Coderlancce <coderlancce@gmail.com>
2022-05-12 09:10:10 -0500

    6 task

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,8 +1,10 @@
 #include "lists.h"
 
 /**
+ * sum_dlistint - sum all elements of the double linked list
+ * @head: pointer to head at doble linked list
  *
- *
+ * Return: the sum of the all elements
  */
 
 int sum_dlistint(dlistint_t *head)
@@ -10,12 +12,12 @@ int sum_dlistint(dlistint_t *head)
 	int sum;
 
 	sum = 0;
-	if(head == NULL)
+	if (head == NULL)
 		return (0);
-	while(head != NULL)
+	while (head != NULL)
 	{
 		sum += head->n;
 		head = head->next;
 	}
-	return(sum);
+	return (sum);
 }
