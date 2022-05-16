dcfd75e5f01a288be48e1f06021d1014abe09a14
Coderlancce <coderlancce@gmail.com>
2022-05-16 16:54:04 -0500

    0 task

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 #define EXIT_SUCCESS 0
 int _putchar(char c);
@@ -23,4 +23,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /*_MAIN_H_*/
+#endif /*MAIN_H*/
